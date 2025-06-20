8b38008ae5ae9486ac0abe39bc2c28d54b1c2853
Ezra Chege <ezra.chege@strathmore.edu>
2025-06-20 09:51:21 +0300

    done

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,3 +1,12 @@
 def bubble_sort(unsorted_list):
-    # TODO: Implement bubble sort
-    pass
+    indexing_length = len(unsorted_list) - 1
+    sorted = False
+
+    while not sorted:
+        sorted = True
+        for i in range(0, indexing_length): 
+            if unsorted_list[i] > unsorted_list[i+1]:
+                sorted = False
+                unsorted_list[i], unsorted_list[i+1] = unsorted_list[i+1], unsorted_list[i]
+
+    return unsorted_list
